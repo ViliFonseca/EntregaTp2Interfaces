35c50e9785a95f6d1a34389ef57204c85ffb7d36
Valentin Fonseca <113689631+ViliFonseca@users.noreply.github.com>
2025-10-02 21:38:55 -0300

    primer comit

---
+++ script.js
@@ -0,0 +1,75 @@
+const menuham = document.getElementById('menuham');
+const toggle = document.getElementById('menu-toggle');
+const overlay = document.getElementById('overlay');
+const portada = document.querySelector('.portada img');
+const leftBtn = document.querySelector('.portada .arrow.left');
+const rightBtn = document.querySelector('.portada .arrow.right');
+const dots = document.querySelectorAll('.portada-dots div');
+
+const portadaImgs = [
+  "img/hollow.jpg",
+  "img/Peg.jpg",
+  "img/GranTurismo.jpg"
+];
+
+let portadaIndex = 0;
+
+function setActiveDot(index) {
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
+
+function mostrarPortada(index) {
+  portada.src = portadaImgs[index];
+  setActiveDot(index);
+}
+
+// flechas
+leftBtn.addEventListener('click', () => {
+  portadaIndex = (portadaIndex - 1 + portadaImgs.length) % portadaImgs.length;
+  mostrarPortada(portadaIndex);
+});
+
+rightBtn.addEventListener('click', () => {
+  portadaIndex = (portadaIndex + 1) % portadaImgs.length;
+  mostrarPortada(portadaIndex);
+});
+
+// dots clickeables
+dots.forEach((dot, i) => {
+  dot.addEventListener('click', () => {
+    portadaIndex = i;
+    mostrarPortada(i);
+  });
+});
+
+// iniciar portada
+mostrarPortada(0);
+
+function setActiveDot(index) {
+  dots.forEach((dot, i) => {
+    dot.classList.toggle('active', i === index);
+  });
+}
+
+setActiveDot(0);
+
+const cerrarMenuham = () => {
+ menuham.classList.remove('open');
+ overlay.classList.remove('show');
+};
+
+toggle.addEventListener('click', () => {
+ const isOpen = menuham.classList.toggle('open');
+ overlay.classList.toggle('show', isOpen);
+});
+
+overlay.addEventListener('click', cerrarMenuham);
+
+window.addEventListener('resize', () => {
+ if (window.innerWidth > 1024) {
+  menuham.classList.remove('open');
+  overlay.classList.remove('show');
+ }
+});
